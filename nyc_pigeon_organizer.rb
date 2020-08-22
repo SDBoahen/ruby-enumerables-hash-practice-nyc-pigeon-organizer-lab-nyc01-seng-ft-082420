faeb3f48a76273357b3057d2f681f44b3f96e6ab
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-22 02:39:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,14 +14,16 @@ def
      
      names.each do |name|
      if  !final_array[name]
-      final_array[name] = {}
-    end
-    if !final_array[name][key]
-      !final_array[name][key] = []
-    end
+          final_array[name] = {}  end
+          
+     if  !final_array[name][key]
+         !final_array[name][key] = []  end
+         
     final_array[name][key].push(inner_key.to_s)
-  end      
-end
+    
+                end
+                
+              end
                                                end
 
 
